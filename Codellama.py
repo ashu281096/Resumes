8ce17a1e4b2d5fac4031a6a6be1bdc6d22b23e4d
Ashutosh Dubey <81738327+ashu281096@users.noreply.github.com>
2024-03-12 11:55:43 +0530

    Update Codellama.py

--- Codellama.py
+++ Codellama.py
@@ -35,3 +35,31 @@ llama_pipeline = pipeline(
     torch_dtype=torch.float16,
     device_map="sequential",
 )
+
+
+
+
+
+
+from transformers import pipeline
+from torch.utils.data import Dataset
+from tqdm.auto import tqdm
+
+pipe = pipeline("text-classification", device=0)
+
+
+class MyDataset(Dataset):
+    def __len__(self):
+        return 5000
+
+    def __getitem__(self, i):
+        return "This is a test"
+
+
+dataset = MyDataset()
+
+for batch_size in [1, 8, 64, 256]:
+    print("-" * 30)
+    print(f"Streaming batch_size={batch_size}")
+    for out in tqdm(pipe(dataset, batch_size=batch_size), total=len(dataset)):
+        pass
